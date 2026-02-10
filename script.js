cb2fa2f0b75253c1822d067db3787c02af91df4a
Lucy <lucymwago9@gmail.com>
2026-02-10 18:32:42 +0300

    add body color

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const card = document.getElementById("placeCard");
 btn.addEventListener("click", () => {
   card.classList.toggle("hidden");
 });
+
+const addPlace = document.getElementById("addPlaceBtn");
+const placeList = document.getElementById("place-list");
+
+addPlace.addEventListener("click", () => {
+  const placeName = document.getElementById("placename").value;
+  const category = document.getElementById("category").value;
+  const date = document.getElementById("date").value;
+  const notes = document.getElementById("notes").value;
+})
