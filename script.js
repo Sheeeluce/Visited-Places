e9accda5674acae3ea1c5f95924d7e727d5cd25e
Lucy <lucymwago9@gmail.com>
2026-02-10 20:51:58 +0300

    a moment of scare

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const btn = document.getElementById("addPlaceBtn");
 const card = document.getElementById("placeCard");
 
+
 btn.addEventListener("click", () => {
   card.classList.toggle("hidden");
 });
-
-const emptyState = document.querySelector(".empty");
-emptyState.style.display = "none";
-
+  
 const addPlace = document.getElementById("addPlaceBtn");
 const placeList = document.getElementById("place-list");
 
 addPlace.addEventListener("click", () => {
-  const placeName = document.getElementById("placename").value;
+  const countryName = document.getElementById("countryname").value;
   const category = document.getElementById("category").value;
   const date = document.getElementById("date").value;
   const notes = document.getElementById("notes").value;
-})
+}) 
+
+
+
