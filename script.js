3bb6b8c192417440ed2104fb9e7cf8a25858bcd9
Lucy <lucymwago9@gmail.com>
2026-02-10 22:04:01 +0300

    adding cards

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("addPlaceBtn");
 const card = document.getElementById("placeCard");
+card.classList.add("placeCard");
 
 
 btn.addEventListener("click", () => {
@@ -11,10 +12,18 @@ const placeList = document.getElementById("place-list");
 
 addPlace.addEventListener("click", () => {
   const countryName = document.getElementById("countryname").value;
+  const location = document.getElementById("location").value;
+  const landmarks = document.getElementById("landmarks").value;
   const category = document.getElementById("category").value;
   const date = document.getElementById("date").value;
   const notes = document.getElementById("notes").value;
 }) 
+
+card.innerHTML = `<h3>${countryName}</h3>
+<p><strong>Category:</strong>${category}</p>
+<p><strong>Location:</strong>${location}</p>
+<p><strong>Date</strong>${date}</p>
+<p><strong>Landmarks:</strong> ${landmarks}</p>`
 
 
 
