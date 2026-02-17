77e502111d1e39cd05a5fe9671911b5ee088fe34
Lucy <lucymwago9@gmail.com>
2026-02-17 20:57:22 +0300

    use of constructors and prototypes

--- script.js
+++ script.js
@@ -1,44 +1,87 @@
 const addBtn = document.getElementById("addPlaceBtn");
 const card = document.getElementById("placeCard");
-const saveBtn = document.getElementById("savePlaceBtn");
-const placeList = document.getElementById("place-list");
-
 
 addBtn.addEventListener("click", () => {
-  card.classList.toggle("hidden");
-});
+   card.classList.toggle("hidden"); 
+  });
+
+function place(countryName, category, location, landmarks, date, notes) {
+  this.countryName = countryName,
+  this.category = category,
+  this.location = location,
+  this.landmarks = landmarks,
+  this.date = date,
+  this.notes = notes
+}
+
+place.prototype.createCard = function () {
+  const newCard = document.createElement("div");
+  newCard.classList.add("place-card")
+
+  newCard.innerHTML = `
+  <h3>${this.countryName}</h3>
+  <p><strong>Category:</strong>${this.category}</p>
   
+  <button class="delete-btn">Delete</button>
+  `;
+  newCard.querySelector("h3").addEventListener("click", () => {
+    this.showDetails();
+  }); 
 
-saveBtn.addEventListener("click", () => {
-  const countryName = document.getElementById("countryname").value;
-  const location = document.getElementById("location").value;
-  const landmarks = document.getElementById("landmarks").value;
-  const category = document.getElementById("category").value;
-  const date = document.getElementById("date").value;
-  const notes = document.getElementById("notes").value;
-
-
-
-
-const newCard = document.createElement("div");
-newCard.classList.add("place-card");
-
-newCard.innerHTML =
-`<h3>${countryName}</h3>
-<p><strong>Category:</strong>${category}</p>
-<p><strong>Location:</strong>${location}</p>
-<p><strong>Date:</strong>${date}</p>
-<p><strong>Landmarks:</strong>${landmarks}</p>
-<p>${notes}</p>
-<button class ="delete-btn">Delete</button>`;
-
-newCard.querySelector(".delete-btn").addEventListener("click", () => {
+  newCard.querySelector(".delete-btn").addEventListener("click", () => {
     newCard.remove();
   });
 
-placeList.appendChild(newCard);
-card.classList.add("hidden");
+return newCard;
+};
+
+place.prototype.showDetails = function () {
+  const placeList = document.getElementById("place-list");
+
+  placeList.innerHTML = `
+  <h2>${this.countryName}</h2>
+  <p><strong>Category:</strong>${this.category}</p>
+  <p><strong>Location📍:</strong>${this.location}</p>
+  <p><strong>Landmarks:</strong>${this.landmarks}</p>
+  <p><strong>Date:</strong>${this.date}</p>
+  <p>${this.notes}</p>
+  `;
+}
+
+const places = [];
+
+const saveBtn = document.getElementById("savePlaceBtn");
+const placeList = document.getElementById("place-list");
+
+saveBtn.addEventListener("click", () => {
+  const newPlace = new place(
+    document.getElementById("countryname").value,
+    document.getElementById("location").value,
+    document.getElementById("landmarks").value,
+    document.getElementById("category").value,
+    document.getElementById("date").value,
+    document.getElementById("notes").value,
+  );
+
+  places.push(newPlace);
+
+
+  const placeCard = newPlace.createCard(place => place.showDetails());
+  placeList.appendChild(placeCard);
+
+  card.classList.add("hidden");
+
+  document.getElementById("countryname").value = "";
+  document.getElementById("location").value = "";
+  document.getElementById("landmarks").value = "";
+  document.getElementById("category").value = "";
+  document.getElementById("date").value = "";
+  document.getElementById("notes").value = "";
 });
+
+
+
+
 
 
 
